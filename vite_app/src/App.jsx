7393ff08f26a6cb6616c506b642ed885908c9042
Atul Mishra <153826399+AtulMishra001@users.noreply.github.com>
2024-08-22 15:56:09 +0000

    things just Started now

---
+++ vite_app/src/App.jsx
@@ -0,0 +1,17 @@
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Navbar from './Navbar'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <Navbar/>
+      </div>
+    </>
+  )
+}
+
+export default App

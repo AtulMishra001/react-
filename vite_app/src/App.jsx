16577660b3f6711af7b904025a9d76d8f57c0b13
Atul Mishra <153826399+AtulMishra001@users.noreply.github.com>
2024-08-30 14:39:57 +0000

    commiting something

--- vite_app/src/App.jsx
+++ vite_app/src/App.jsx
@@ -1,7 +1,4 @@
-
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Navbar from './Navbar'
+import Navbar from './assets/components/Navbar'
 import './App.css'
 
 function App() {
